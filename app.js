072f2928c200a9fe7c4479565656bee8bafbb0cf
catherineisonline <ekaterine.jpg@gmail.com>
2022-01-20 11:42:20 +0400

    updated css

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-'use strict';
-const myNav = document.querySelector('.navigation');
-const myHamburger =  document.querySelector('.ham-icon');
-const menuItem = document.querySelectorAll('.menu-item');
+"use strict";
+const myNav = document.querySelector(".navigation");
+const myHamburger = document.querySelector(".ham-icon");
+const menuItem = document.querySelectorAll(".menu-item");
+const hamIcon = document.querySelector(".fa-bars");
 
-console.log( menuItem);
+console.log(menuItem);
 
-
-myHamburger.addEventListener('click', function() {
-        myNav.classList.toggle('responsive');
-  
+myHamburger.addEventListener("click", function () {
+  myNav.classList.toggle("responsive");
+  if (myNav.classList.contains("responsive")) {
+    hamIcon.classList.remove("fa-bars");
+    hamIcon.classList.add("fa-times");
+  } else {
+    hamIcon.classList.add("fa-bars");
+    hamIcon.classList.remove("fa-times");
+  }
 });
 
-menuItem.forEach(function(e){
-    e.addEventListener('click', function(){
-        myNav.classList.remove('responsive');
-    })
-})
-
-
+menuItem.forEach(function (e) {
+  e.addEventListener("click", function () {
+    myNav.classList.remove("responsive");
+  });
+});
